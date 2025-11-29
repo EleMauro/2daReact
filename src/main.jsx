30e192e66b4a8d8aaddd9ee4ea12b2d71319c880
EleMauro <lucas.mauro1973@gmail.com>
2025-11-29 16:29:55 -0300

    Fix: remove basename for Vercel deploy

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { CartProvider } from "./context/CartContext.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CartProvider>
-      <BrowserRouter basename="/reactnuevo">
+      <BrowserRouter>
         <App />
       </BrowserRouter>
     </CartProvider>
